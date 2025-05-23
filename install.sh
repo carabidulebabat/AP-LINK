880685d1f975684dd46a86fba2d8527da1224024
cara <129581427+carabidulebabat@users.noreply.github.com>
2025-05-23 22:26:05 +0200

    Update install.sh
    need to write ip addr show and grep nic interface.
    Once done, will be only python code, got draft, feel free to contribute.
    It use iw for set tx power, a guide for explanation of tx power, channel, dfs will come in this repo
    Its day1

--- install.sh
+++ install.sh
@@ -1,53 +1,77 @@
-!/bin/bash
+#!/bin/bash
 
+set -e
 
-if [["$EUID" -ne 0]]; then
-    echo "run as root"e
-    exit 1
-fi
+# Variables personnalisables
+SSID="VTX-FLY"
+PASSPHRASE="12345678"
+WIFI_INTERFACE="wlx0013ef500122" #need to be change with nic name
+INTERNET_INTERFACE="eth0"
+## downloads aircrack ng or wifi driver that you want ###
 
-if [["$#" -ne 4]]; then
-    echo "need ssid, domain, ghz, chan"
-    exit 1
-fi
+echo "Installation des dépendances"
+sudo apt update
+sudo apt install -y build-essential libssl-dev libnl-3-dev libnl-genl-3-dev pkg-config git dnsmasq iptables dkms
 
-$SSID=$1
-$DOMAIN=$2
-$ghz=$3
-chan=$4
+git clone git clone -b v5.6.4.2 https://github.com/aircrack-ng/rtl8812au.git
+cd rtl8812au
+sudo make dkms_install
 
+sudo apt update
+sudo apt install -y build-essential libssl-dev libnl-3-dev libnl-genl-3-dev pkg-config git dnsmasq iptables dkms
 
-echo "Installing packages..."
-apt update && apt install -y hostapd iw wpa_supplicant dnsmasq
+echo "Téléchargement et compilation de hostapd"
+cd /usr/local/src
+sudo git clone https://w1.fi/hostap.git || true
+cd hostap/hostapd
+sudo cp defconfig .config
 
-echo "Stopping services while we configure them..."
-systemctl stop hostapd
-systemctl stop dnsmasq
+# Activer nl80211
+sudo sed -i 's/#CONFIG_DRIVER_NL80211=y/CONFIG_DRIVER_NL80211=y/' .config
 
-echo "Writing hostapd.conf..."
-cat > /etc/hostapd/hostapd.conf <<EOF
-interface=wlan0
+# Compilation
+sudo make
+sudo make install
+
+ip addr show 
+
+echo "Création du fichier de configuration hostapd"
+sudo bash -c "cat > /usr/local/etc/hostapd.conf <<EOF
+interface=$WIFI_INTERFACE
 driver=nl80211
 ssid=$SSID
-hw_mode=$ghz
-channel=$chan
+hw_mode=g
+channel=7
+wmm_enabled=0
+macaddr_acl=0
 auth_algs=1
+ignore_broadcast_ssid=0
+wpa=1
+wpa_passphrase=$PASSPHRASE
+wpa_key_mgmt=WPA-PSK
 rsn_pairwise=CCMP
-country_code=$DOMAIN
-EOF
+EOF"
 
-echo "hostapd is starting on $ghz, channel $chan, avaible on network with $SSID"
+echo "[4/7] Configuration de dnsmasq (DHCP)"
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak || true
+sudo bash -c "cat > /etc/dnsmasq.conf <<EOF
+interface=$WIFI_INTERFACE
+dhcp-range=192.168.50.10,192.168.50.100,255.255.255.0,24h
+EOF"
 
-systemctl start hostpad
-systemctl start dnsmasq
+echo "[5/7] Attribution IP statique à $WIFI_INTERFACE"
+sudo ip addr add 192.168.50.1/24 dev $WIFI_INTERFACE
+sudo ip link set $WIFI_INTERFACE up
 
-echo "start successfully will configure rtsp udp ip"
-echo "..."
-echo ".."
-echo "."
+echo "[6/7] Activation du routage NAT"
+sudo bash -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+sudo iptables -t nat -A POSTROUTING -o $INTERNET_INTERFACE -j MASQUERADE
+sudo iptables -A FORWARD -i $INTERNET_INTERFACE -o $WIFI_INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i $WIFI_INTERFACE -o $INTERNET_INTERFACE -j ACCEPT
 
-echo "set iw setting, like txpower"
+echo "[7/7] Démarrage des services"
+sudo dnsmasq
+sudo /usr/local/bin/hostapd /usr/local/etc/hostapd.conf &
 
-iw wlan0 set tx power 20
+echo "✅ Point d'accès Wi-Fi '$SSID' actif via hostapd compilé à partir des sources."
 
- 
