d5702eb13612bbf23c33a31fed406abc72f20954
cara <129581427+carabidulebabat@users.noreply.github.com>
2025-05-26 20:55:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 set -e
 SSID="VTX-FLY"
-WIFI_INTERFACE="wlx0013ef500122" #need to be change with nic name
+WIFI_INTERFACE="wlan0/1" need to be change
 ## downloads aircrack ng or wifi driver that you want ###
 ######chmod to files#####
 
@@ -26,34 +26,16 @@ sleep 2
 
 
 echo "download of hostapd"
-cd /usr/local/src
 sudo git clone https://w1.fi/hostap.git || true
-cp defconfig /usr/local/src/hostap/hostapd
+cp defconfig /hostap/hostapd/.config
 cd hostap/hostapd
-sudo cp defconfig .config
-
-#enable driver
-sudo cpd
-
+echo"make...."
 sudo make
-sudo make install
-
-WIFI_INTERFACE=$(ip -o link show | awk -F': ' '/wl/ {print $2; exit}')
+#sudo make install
 
 ##START ON CHANNEL 36 5.8GHZ## ON AP channel are set auto on gs, with tx power of 20dbm so 100mw, ACS enable, will pick the best channel avaible###
-echo "Création du fichier de configuration hostapd"
-sudo bash -c "cat > /etc/hostapd/hostapd.conf <<EOF
-interface=$WIFI_INTERFACE
-hw_mode=a
-channel=0
-country_code=US
-# 802.11ac support
-ieee80211ac=1
-wmm_enabled=1
-ssid=VTX
-auth_algs=1
-EOF"
-
+echo "copy of hostapd.conf to /etc/hostapd/hostapd.conf....."
+sudo cp hostapd.conf /etc/hostapd/hostapd.conf 
 echo "WIFI AP '$SSID' '$WIFI_INTERFACE' hostapd compile successfully, download of dnsmasq."
 
 sudo apt update
@@ -66,7 +48,7 @@ sudo ip link set $WIFI_INTERFACE down
 sudo ip addr add 192.168.0.1/24 dev $WIFI_INTERFACE
 sudo ip link set $WIFI_INTERFACE up
 
-sudo systemctl restart dnsmasq
+sudo systemctl enable dnsmasq
 
 echo 'stream is launching, hostapd is working, end of code' 
 
