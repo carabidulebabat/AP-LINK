c78b2a0d2bfef80708ceacbf6a93909569ed03a3
cara <129581427+carabidulebabat@users.noreply.github.com>
2025-05-26 20:58:03 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,15 +1,14 @@
 !/bin/bash
 
 #INIT HOSTAPD, SET STATIC IP#
+interface="interface"
 
-sudo ip link set wlx0013ef500122 down
-sudo iw dev wlx0013ef500122 set type __ap
-sudo ip link set wlx0013ef500122 up
+sudo ip link set $interface down
+sudo iw dev $interface set type __ap
+sudo ip link set $interface up
 
-echo "SET IP FIX AT wlx0013ef500122"
-sudo ip link set wlx0013ef500122 down
-sudo ip addr add 192.168.0.1/24 dev wlx0013ef500122
-sudo ip link set wlx0013ef500122 up
+sudo ip link set $interface down
+sudo ip addr add 192.168.0.1/24 dev $interface
+sudo ip link set $interface up
 
 sudo hostapd /etc/hostapd/hostapd.conf
-ffmpeg -re -f v4l2 -i /dev/video0 -r 10 -f mpegts -fflags nobuffer -flags low_delay udp://192.168.0.131:8554?pkt_size=1316
